f4345d8d42d523a9ef3a4345f4df657f41e49a51
dwebb1 <danielwebb@hate.cf>
2017-12-26 22:03:25 +1100

    Fix db.php to show result after db connection PDO

--- db.php
+++ db.php
@@ -1,3 +1,16 @@
 <?php
-    $myPDO = new PDO('mysql:host=172.30.197.234;dbname=wpdb', 'user6G8', 'WBFovxEkJOBIdwPc');
+$servername = "172.30.197.234";
+$username = "user6G8";
+$password = "WBFovxEkJOBIdwPc";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=wpdb", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
 ?>
